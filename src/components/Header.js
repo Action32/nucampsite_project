07e6a41cfe569a39d5feff267904339855cba542
Igor Mayer <goldenmaer32@gmail.com>
2024-04-10 11:37:00 -0500

    campsitesSlice]

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from '../app/assets/img/logo.png';
+
+const Header = () => {
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+              
+          </Navbar>
+    );
+};
+
+export default Header;
